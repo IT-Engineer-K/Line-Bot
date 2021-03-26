dd9e9d039c48c435bffb549fceecdbdb97eacc72
MagicianK <76894075+MagicianK@users.noreply.github.com>
2021-03-26 12:47:57 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import json
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
+import datetime
+def format_date(date):
+    __WEEKDAY = ('月','火','水','木','金','土','日')
+    return "%s" % (__WEEKDAY[date.weekday()])
+
+now = datetime.datetime.now()
+print(now.hour)
+if now.hour < 12:
+    time = "朝"
+else:
+    time = "夜"
 
 file = open('info.json', 'r')
 info = json.load(file)
@@ -10,7 +21,7 @@ line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 
 def main():
     USER_ID = info['USER_ID']
-    messages = TextSendMessage(text = "おはよう")
+    messages = TextSendMessage(text = format_date(datetime.date.today()) + "曜日の" + time + "です。\n薬飲んだ？")
     line_bot_api.push_message(USER_ID, messages=messages)
     
 if __name__ == "__main__":
